44494d485cf3af05fd6af29257773afd37abdfe2
kali1402 <amcopeew68@naver.com>
2022-09-07 00:41:56 +0900

    스테이지 1준비중

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Stage0 from "./stage0/Main";
+import App from "./App";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        {/* <App /> */}
+        <Stage0 />
+    </React.StrictMode>
+);
---
+++ src/stage0/Main.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function Main() {
+    const [count, setCount] = useState(0);
+
+    return <div className="stage0">stage0</div>;
+}
+
+export default Main;
